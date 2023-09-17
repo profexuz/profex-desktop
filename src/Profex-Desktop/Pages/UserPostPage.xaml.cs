46ec142594a2b685f76607075412e3659eb6fcef
AzimJan <azimjonjovliev@gmail.com>
2023-09-17 21:45:26 +0500

    UserPostPage created

---
+++ src/Profex-Desktop/Pages/UserPostPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Profex_Desktop.Pages
+{
+    /// <summary>
+    /// Interaction logic for UserPostPage.xaml
+    /// </summary>
+    public partial class UserPostPage : Page
+    {
+        public UserPostPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
