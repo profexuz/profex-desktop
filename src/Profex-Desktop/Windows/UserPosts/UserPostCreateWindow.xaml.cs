efb996dc493d6b171758532f6e83b90bcc1ead5b
AzimJan <azimjonjovliev@gmail.com>
2023-09-19 13:39:07 +0500

    Changes have been made to USERPOSTPAGE and USERPOSTCREATE

--- src/Profex-Desktop/Windows/UserPosts/UserPostCreateWindow.xaml.cs
+++ src/Profex-Desktop/Windows/UserPosts/UserPostCreateWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using Profex_Integrated.Services.Vacancies;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace Profex_Desktop.Windows.UserPosts
 {
@@ -20,42 +8,48 @@ namespace Profex_Desktop.Windows.UserPosts
     /// </summary>
     public partial class UserPostCreateWindow : Window
     {
-        private VacancyService vacancyService = new VacancyService();
-        public long vacancyId;
-        private string BASE_URL = "htpp://64.227.42.134.4040";
         public UserPostCreateWindow()
         {
             InitializeComponent();
         }
 
-        private void btnClose_Click(object sender, RoutedEventArgs e)
+        private void btnCreateWindowClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+
+        private void btnPicture_MouseDown(object sender, MouseButtonEventArgs e)
         {
 
         }
 
-        private void rbImg1_Click(object sender, RoutedEventArgs e)
+        private void btnPicture2_MouseDown(object sender, MouseButtonEventArgs e)
         {
 
         }
 
-        private void rbImg2_Click(object sender, RoutedEventArgs e)
+
+        private void btnPicture3_MouseDown(object sender, MouseButtonEventArgs e)
         {
 
         }
 
-        private void rbImg3_Click(object sender, RoutedEventArgs e)
+        private void btnPicture4_MouseDown(object sender, MouseButtonEventArgs e)
         {
 
         }
 
-        private void rbImg_Click(object sender, RoutedEventArgs e)
+        private void btnPicture5_MouseDown(object sender, MouseButtonEventArgs e)
         {
 
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        private void btnSave_Click(object sender, RoutedEventArgs e)
         {
 
         }
+
+
     }
 }
