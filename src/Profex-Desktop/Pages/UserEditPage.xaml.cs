e10048bc50b27048e1788bf68e98f50af2b6b6fd
AzimJan <azimjonjovliev@gmail.com>
2023-09-19 22:22:02 +0500

    Revised the USER post, fixed the post creation and modification section

--- src/Profex-Desktop/Pages/UserEditPage.xaml.cs
+++ src/Profex-Desktop/Pages/UserEditPage.xaml.cs
@@ -1,7 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http.Internal;
+using Profex_Dtos.Masters;
+using Profex_Dtos.Users;
+using Profex_Integrated.Security;
+using Profex_Integrated.Services.Auth.JwtToken;
+using Profex_Integrated.Services.Users;
+using Profex_ViewModels.Masters;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,21 +29,16 @@ namespace Profex_Desktop.Pages
     /// </summary>
     public partial class UserEditPage : Page
     {
+        private string _path = "C:\\Users\\Public\\Token.txt";
+        private UserService _userService = new UserService();
+        private JwtParser jwtParser = new JwtParser();
+        private UserUpdateDto _userViewModel = new UserUpdateDto();
+        private string selectedFilePath = "";
+        private string BASEIMG_URL = "http://64.227.42.134:4040/";
         public UserEditPage()
         {
             InitializeComponent();
         }
-
-        private void btnCreate_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void Page_Loaded(object sender, RoutedEventArgs e)
-        {
-
-        }
-
         private void txtNum_TextChanged(object sender, TextChangedEventArgs e)
         {
 
@@ -42,32 +46,135 @@ namespace Profex_Desktop.Pages
 
         private void brUpload_MouseDown(object sender, MouseButtonEventArgs e)
         {
-                
+            var clickedElement = sender as UIElement;
+
+            if (clickedElement != null)
+            {
+                // OpenFileDialog obyektini yaratish
+                Microsoft.Win32.OpenFileDialog openFileDialog = new Microsoft.Win32.OpenFileDialog();
+
+                // Foydalanuvchi faylni tanlashi mumkin bo'lgan fayl formatlari
+                openFileDialog.Filter = "Rasm fayllari|*.jpg;*.jpeg;*.png;|Barcha fayllar|*.*";
+
+                // Faylni tanlash va natijani oluvchi oynani ochish
+                bool? result = openFileDialog.ShowDialog();
+
+                // Agar fayl tanlansa va rasm fayli bo'lsa
+                if (result == true)
+                {
+                    selectedFilePath = openFileDialog.FileName;
+                    if (selectedFilePath.Contains(".jpg") | selectedFilePath.Contains(".jpeg") | selectedFilePath.Contains(".png"))
+                    {
+                        btnSave.IsEnabled = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Faqat 'jpg','jpeg' va 'png' formatdagi rasmlarni yuklay olasiz", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        btnSave.IsEnabled = false;
+                    }
+
+                    // Tanlangan rasm faylini olish va kerakli ishlar bilan davom etish
+                    // Misol uchun: Tanlangan rasmni bir joyga joylash va uni ko'rsatish
+                    ImageSource imageSource = new BitmapImage(new Uri(selectedFilePath));
+                    imgProfile.ImageSource = imageSource;
+                }
+            }
         }
 
         private void btnCancel_Click(object sender, RoutedEventArgs e)
         {
+            txtFName.IsReadOnly = false;
+            txtLName.IsReadOnly = false;
+            txtNum.IsReadOnly = false;
+            cmbIsFree.IsReadOnly = false;
 
+            btnSave.Visibility = Visibility.Hidden;
+            btnCancel.Visibility = Visibility.Hidden;
+            btnChange.Visibility = Visibility.Visible;
         }
 
         private void btnChange_Click(object sender, RoutedEventArgs e)
         {
+            btnSave.Visibility = Visibility.Visible;
+            btnCancel.Visibility = Visibility.Visible;
+            btnChange.Visibility = Visibility.Hidden;
+            brUpload.IsEnabled = true;
+            
+            txtFName.IsReadOnly = false;
+            txtLName.IsReadOnly = false;
+            txtNum.IsReadOnly = false;
+            cmbIsFree.IsReadOnly = false;
+        }
+
+        private async void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            btnSave.Visibility = Visibility.Visible;
+            btnCancel.Visibility = Visibility.Visible;
+            btnChange.Visibility = Visibility.Hidden;
+
+
+            string token = File.ReadAllText(_path);
+            IdentityService identityService = jwtParser.ParseToken(token);
+
+            _userViewModel.FirstName = txtFName.Text;
+            _userViewModel.LastName = txtLName.Text;
+            _userViewModel.PhoneNumber = "+" + txtNum.Text;
+            
+            if (!string.IsNullOrEmpty(selectedFilePath))
+            {
+                // Faylni IFormFile ko'rinishida yaratish
+                var fileBytes = File.ReadAllBytes(selectedFilePath);
+                var fileName = System.IO.Path.GetFileName(selectedFilePath);
+                _userViewModel.ImagePath = new FormFile(new MemoryStream(fileBytes), 0, fileBytes.Length, null, fileName);
+                btnSave.IsEnabled = true;
+            }
+            else
+            {
+                var res = await _userService.GetByIdAsync(IdentitySingelton.GetInstance().Id);
+
+                // _masterViewModel.ImagePath ga ro'yxatni o'rnating
+                _userViewModel.ImagePath = new FormFile(new MemoryStream(Encoding.UTF8.GetBytes(res.ImagePath)), 0, res.ImagePath.Length, null, System.IO.Path.GetFileName(res.ImagePath));
+                btnSave.IsEnabled = true;
+            }
+            
+            var result = await _userService.UpdateAsync(identityService.Id, _userViewModel);
+            if (result == true)
+            {
+                txtFName.IsReadOnly = true;
+                txtLName.IsReadOnly = true;
+                txtNum.IsReadOnly = true;
+                //cmbIsFree.IsReadOnly = true;
+
+                btnSave.Visibility = Visibility.Hidden;
+                btnCancel.Visibility = Visibility.Hidden;
+                btnChange.Visibility = Visibility.Visible;
+                UserMainWindow main = new UserMainWindow();
+                main.Show();
+            }
+            else
+            {
+                MessageBox.Show("Internet bilan muammo yuzaga keldi!");
+            }
 
         }
 
-        private void btnSave_Click(object sender, RoutedEventArgs e)
+        private async void Page_loaded(object sender, RoutedEventArgs e)
         {
-
-        }
-
-        private void Page_loaded(object sender, RoutedEventArgs e)
-        {
-
+            string token = File.ReadAllText(_path);
+            IdentityService identityService = jwtParser.ParseToken(token);
+            var result = await _userService.GetByIdAsync(identityService.Id);
+            txtFName.Text = result.FirstName.ToUpper();
+            txtLName.Text = result.LastName.ToUpper();
+            txtNum.Text = result.PhoneNumber.ToUpper().Substring(1);
+            string imageUrl = BASEIMG_URL + result.ImagePath;
+            Uri imageUri = new Uri(imageUrl, UriKind.Absolute);
+            imgProfile.ImageSource = new BitmapImage(imageUri);
         }
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
         }
     }
 }
--- src/Profex-Desktop/Windows/UserPosts/UserPostCreateWindow.xaml.cs
+++ src/Profex-Desktop/Windows/UserPosts/UserPostCreateWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows;
+﻿
+using Profex_Integrated.Interfaces;
+using Profex_Integrated.Services.Posts;
+using Profex_Integrated.Services.Skills;
+using Profex_ViewModels.Skills;
+using Profex_ViewModels.Vacancies;
+using System.Windows;
 using System.Windows.Input;
 
 namespace Profex_Desktop.Windows.UserPosts
@@ -8,9 +14,40 @@ namespace Profex_Desktop.Windows.UserPosts
     /// </summary>
     public partial class UserPostCreateWindow : Window
     {
+        
+        public long categoryId;
+        private PostService _postService = new PostService();
+        
         public UserPostCreateWindow()
         {
             InitializeComponent();
+        }
+        /*public void SetData(SkillViewModel skill)
+        {
+            if (skill != null)
+            {
+                NameOfSkill.Content = skill.Title;
+                categoryId = skill.CategoryId;
+                SkillId = skill.Id;
+            }
+            else
+            {
+                MessageBox.Show("Ma'lumotlar topilmadi");
+            }
+        }*/
+        public void SetData(Vacancy vacancy)
+        {
+            if(vacancy!=null)
+            {
+                
+                categoryId = vacancy.CategoryId;
+                lblPostTitle.Content = vacancy.Title;
+                lblPrice.Content = vacancy.Price.ToString();
+                lblUserRegion.Content = vacancy.Region;
+                lblUserDistrict.Content = vacancy.District;
+                txtPhoneNumber.Text = vacancy.PhoneNumber.ToString();
+                rbDefenation.Text = vacancy.Description;
+            }
         }
 
         private void btnCreateWindowClose_Click(object sender, RoutedEventArgs e)
@@ -21,7 +58,7 @@ namespace Profex_Desktop.Windows.UserPosts
 
         private void btnPicture_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            
         }
 
         private void btnPicture2_MouseDown(object sender, MouseButtonEventArgs e)
@@ -45,11 +82,29 @@ namespace Profex_Desktop.Windows.UserPosts
 
         }
 
-        private void btnSave_Click(object sender, RoutedEventArgs e)
+        private async void btnSave_Click(object sender, RoutedEventArgs e)
         {
+            try
+            {
+                var result = await _postService.AddSkill(categoryId);
 
+                if (result == 1)
+                {
+                    MessageBox.Show("Skill added successfully!");
+                }
+                else if (result == 0)
+                {
+                    MessageBox.Show("skill has already exists.");
+                }
+                else if (result == -1)
+                {
+                    MessageBox.Show("An error occurred while adding the skill.");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("internet is slow!");
+            }
         }
-
-
     }
 }
