4065feb2fbafd7a868dc2a5a57df69c14956afa6
AzimJan <azimjonjovliev@gmail.com>
2023-09-19 13:32:22 +0500

    UserViewWindow created

--- src/Profex-Desktop/Components/UserPosts/UserPostxaml.xaml.cs
+++ src/Profex-Desktop/Components/UserPosts/UserPostxaml.xaml.cs
@@ -1,17 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Profex_Desktop.Windows.UserPosts;
+using Profex_Integrated.Services.Skills;
+using Profex_Integrated.Services.Vacancies;
+using Profex_ViewModels.Vacancies;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Profex_Desktop.Components.UserPosts
 {
@@ -20,6 +13,11 @@ namespace Profex_Desktop.Components.UserPosts
     /// </summary>
     public partial class UserPostxaml : UserControl
     {
+        public  long UserId { get; set; }
+        public long PostId { get; set; }
+        //private SkillsService _skillsService = new SkillsService();
+        private VacancyService _vck = new VacancyService();
+
         public UserPostxaml()
         {
             InitializeComponent();
@@ -27,12 +25,56 @@ namespace Profex_Desktop.Components.UserPosts
 
         private void btnCreate_Click(object sender, RoutedEventArgs e)
         {
-
+            UserPostUpdateWindow msk = new UserPostUpdateWindow();
+            msk.ShowDialog();
         }
 
         private void grMain_MouseDown(object sender, MouseButtonEventArgs e)
         {
 
         }
+
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var result = await _vck.RemoveAsync(PostId);
+
+                // Check the result and take appropriate action.
+                if (result == 1)
+                {
+                    // Skill added successfully, you can update your UI here if needed.
+                    MessageBox.Show("Skill removed successfully!");
+                }
+                else if (result == 0)
+                {
+                    // Handle the case where adding the skill failed.
+                    MessageBox.Show("skill has already removed.");
+                }
+                else if (result == -1)
+                {
+                    // Handle unexpected errors.
+                    MessageBox.Show("An error occurred while removed the skill.");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("internet is slow!");
+            }
+        }
+        public void SetData(Vacancy vacancy )
+        {
+            if (vacancy != null)
+            {
+                lbName.Content = vacancy.Title;
+                UserId = vacancy.UserId;
+                PostId = vacancy.Id;
+            }
+            else
+            {
+                MessageBox.Show("Ma'lumotlar topilmadi");
+            }
+        }
+
     }
 }
---
+++ src/Profex-Desktop/Windows/UserPosts/UserPostviewWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using Profex_Integrated.Services.Vacancies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Profex_Desktop.Windows.UserPosts
+{
+    /// <summary>
+    /// Interaction logic for UserPostCreateWindow.xaml
+    /// </summary>
+    public partial class UserPostviewWindow : Window
+    {
+        private VacancyService vacancyService = new VacancyService();
+        public long vacancyId;
+        private string BASE_URL = "htpp://64.227.42.134.4040";
+        public UserPostviewWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbImg1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbImg2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbImg3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rbImg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
